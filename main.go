044962612ae655058938ca21df26325368dcbfe6
rickyhidayatt <rickyhyouth@gmail.com>
2023-02-20 11:45:38 +0700

    succses open connection

--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
 
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "Repsol12"
+	dbname   = "incubation_class"
+)
+
+type Customer struct {
+	Id      string
+	Name    string
+	Balance int
+}
+
+var connectDB = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+func main() {
+	db, err := sql.Open("postgres", connectDB)
+	defer db.Close()
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	if err := db.Ping(); err != nil {
+		fmt.Println(err.Error())
+	}
+
+	newCustomer := Customer{
+		Name:    "Dicky Tanda tanya",
+		Balance: 1029292,
+	}
+
+	insertData(db, newCustomer)
+}
+
+func insertData(db *sql.DB, newCustomer Customer) {
+	query := "INSERT INTO customer (name, balance) VALUES ($1, $2)"
+	result, err := db.Exec(query, newCustomer.Name, newCustomer.Balance)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(result)
 }
